cb23fa61bffc70fd301a004a0755d2cf9e42ee0e
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2020-06-12 08:35:19 -0400

    Update print statements + license date

--- src/pata_password_cracker/__main__.py
+++ src/pata_password_cracker/__main__.py
@@ -43,7 +43,7 @@ def process_input(yaml_file, words_file, plugins):
     Create a new YAML parsing object
     and dump the content out as a dict
     """
-    print "Processing input YAML"
+    print ("Processing input YAML")
     yaml_to_dict = ProcessInputYaml()
     yaml_to_dict = yaml_to_dict.yaml_processor(yaml_file)
     words_to_list = ProcessInputWords()
